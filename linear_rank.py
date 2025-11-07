40cfcc9f068af3e7287989615daf5d4ea2c62ff3
OliverSell <s204692@student.dtu.dk>
2025-11-07 19:32:35 +0100

    Adding start of linear transformation

--- linear_rank.py
+++ linear_rank.py
@@ -31,5 +31,36 @@ print("\nTop off-targets by predicted activity:\n")
 print(ranked[['name', 'offtarget_sequence', 'predicted_activity', 'experimental_activity']])
 
 
+# Depending on the transformer outpt. In this case I put dummy variables, of 4 and 768? ###  To be changed
+batch_size = 4 
+latent_dim = 768
+transformer_output = torch.randn(batch_size, latent_dim)
+
+# Define linear layer (fully connected). Out feature is just activity, so dimension is 1.
+activity_head = nn.Linear(in_features=latent_dim, out_features=1)
+
+# Run the NN
+predicted_activity = activity_head(transformer_output)
+
+print("predicted activity shape: ", predicted_activity.shape)
+
+
+
+# Could also look something like this, if we have a classs structure # I think we do.
+class OffTargetPredictor(nn.Module):
+    def __init__(self, input_dim):
+        super().__init__()
+        self.fc = nn.Linear(input_dim, 1)
+    
+    def forward(self, x):
+        return self.fc(x)
+
+model = OffTargetPredictor(input_dim=768)
+x = torch.randn(10, 768)  # e.g., transformer outputs for 10 off-targets
+y_pred = model(x)
+print("Predicted y_pred.shape: ", y_pred.shape)  # torch.Size([10, 1])
+
+
+
 
 
